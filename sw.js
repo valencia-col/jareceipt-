372845038a663e331753d33e188ef5c0c07831c1
valencia-col <yolaimer@gmail.com>
2026-05-09 22:20:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,31 @@
-self.addEventListener('fetch', function(event) {
-    // Este archivo permite que la App sea instalable
+const CACHE_NAME = 'ja-receipt-v2';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'icon.png',
+  'icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
+  'https://cdn.jsdelivr.net/npm/html2canvas@1.4.1/dist/html2canvas.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
 });
